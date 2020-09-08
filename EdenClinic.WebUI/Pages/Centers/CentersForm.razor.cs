4bd0023ecdbf2c3463dacf912a51062d42b2ba2b
Amir Imam Mohamed <ai_1583@hotmail.com>
2020-09-08 20:25:37 +0300

    WebUI: submit material design

---
+++ EdenClinic.WebUI/Pages/Centers/CentersForm.razor.cs
@@ -0,0 +1,18 @@
+﻿using EdenClinic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EdenClinic.WebUI.Pages.Centers
+{
+    public partial class CentersForm
+    {
+        private Center Model = new Center();
+
+        private async void Submit()
+        {
+
+        }
+    }
+}
